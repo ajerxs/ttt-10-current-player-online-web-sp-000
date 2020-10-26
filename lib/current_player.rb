66f52ef7e224f4f9062b31d544a3487e56d63fc7
Andrew Eames <ajerxs@gmail.com>
2020-10-26 00:34:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,8 +3,9 @@ def turn_count(board)
   counter = 0
   board.each do |turn|
     if turn == "X" || turn == "O"
-      puts "#{counter}"
       counter += 1
+      puts "#{counter}"
     end
   end
+  counter
 end
